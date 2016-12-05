8b2e21a0966d09b30d38a7af9bca0c10c2a0b70e
Thomas Kindler <virtualdreamsdd@aol.com>
2016-12-05 15:01:12 +0100

    Added gulp. Moved css and js to lib folder.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require("gulp"),
+	concat = require("gulp-concat"),
+	rename = require("gulp-rename"),
+	cssmin = require("gulp-cssmin"),
+	less = require("gulp-less"),
+	uglify = require("gulp-uglify")
+
+var cssTargetDir = 'wwwroot/css/'
+var jsTargetDir = 'wwwroot/js/'
+
+gulp.task('notes-css', function() {
+	return gulp.src([
+		'Assets/css/notes.less'
+	])
+	.pipe(less())
+	.pipe(concat('notes.min.css'))
+	.pipe(cssmin())
+	.pipe(gulp.dest(cssTargetDir));
+});
+
+gulp.task('admin2-js', function() {
+	return gulp.src([
+		'Assets/js/sb-admin-2.js'
+	])
+	.pipe(uglify())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest(jsTargetDir))
+});
+
+gulp.task('notes-js', function() {
+	return gulp.src([
+		'Assets/js/notes.js'
+	])
+	.pipe(uglify())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest(jsTargetDir))
+});
+
+gulp.task('default', [
+	'notes-css',
+	'admin2-js',
+	'notes-js'
+]);
