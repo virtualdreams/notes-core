282e4b1b46c94d30ba3ec2bd67a4cb51d2e1e38f
Thomas Kindler <virtualdreamsdd@aol.com>
2016-12-05 15:05:42 +0100

    Moved click handler to glaobal js.

--- Assets/js/notes.js
+++ Assets/js/notes.js
@@ -1,4 +1,9 @@
 $(function() {
+	$('[data-href]').click(function() {
+		var href = $(this).data('href');
+		location.href = href;
+	});
+	
 	$('#selectAll').click(function () {
 		$('input[name=id][type=checkbox]').prop('checked', function () {
 			if ($('#selectAll').is(':checked')) {
