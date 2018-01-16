2182fc508bc450ac69028a3401d44f90f1c6722f
Thomas Kindler <virtualdreamsdd@aol.com>
2018-01-16 12:01:21 +0100

    Added fix for startup error.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
+using System;
 using System.IO;
 
 namespace notes
@@ -11,6 +12,7 @@ namespace notes
 	{
 		public static void Main(string[] args)
 		{
+			new ArgumentException();
 			BuildWebHost(args).Run();
 		}
 
