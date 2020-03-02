7808e44d0b4429863a2cdd16f4986a1e9b4d2429
Thomas Kindler <virtualdreamsdd@aol.com>
2020-03-02 16:39:46 +0100

    Multiple occurences of FormatDate removed (DRY).
    Moved FormatDate in to a ViewComponent.

---
+++ ViewComponents/DateViewComponent.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using notes.Core.Services;
+
+namespace notes.ViewComponents
+{
+	public class FormatDateViewComponent : BaseViewComponent
+	{
+		private readonly UserService UserService;
+
+		public FormatDateViewComponent(UserService user)
+			: base(user)
+		{
+			UserService = user;
+		}
+
+		public IViewComponentResult Invoke(DateTime date)
+		{
+			var _dt = String.Format(new System.Globalization.CultureInfo("en-US"), "{0:ddd, dd MMM yyyy HH:mm:ss}", date);
+
+			return Content($"{_dt}");
+		}
+	}
+}
