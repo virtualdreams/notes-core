e4782ee4f30a21664cbe49d8bd71616d001f624f
Thomas Kindler <virtualdreamsdd@aol.com>
2020-03-09 16:47:07 +0100

    TagHelper reworked.
    The content of the tag can now be passed directly via an attribute.
    A new inline helper was introduced for backward compatibility.

--- TagHelpers/MarkdownTagHelper.cs
+++ TagHelpers/MarkdownTagHelper.cs
@@ -1,24 +1,36 @@
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
 using notes.Extensions;
 
 namespace notes.TagHelpers
 {
-	[HtmlTargetElement("markdown")]
-	[HtmlTargetElement("p", Attributes = "markdown")]
+	[HtmlTargetElement("markdown", Attributes = "content")]
 	public class MarkdownTagHelper : TagHelper
+	{
+		public string Content { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = null; // remove tag
+
+			var _markdown = Content?.ToMarkdown();
+
+			output.Content.SetHtmlContent(_markdown ?? String.Empty);
+		}
+	}
+
+	[HtmlTargetElement("markdown-inline")]
+	public class MarkdownInlineTagHelper : TagHelper
 	{
 		public override void Process(TagHelperContext context, TagHelperOutput output)
 		{
-			if (output.TagName.Equals("markdown"))
-				output.TagName = null;
-
-			output.Attributes.RemoveAll("markdown");
+			output.TagName = null; // remove tag
 
 			var _content = output.GetChildContentAsync().Result.GetContent();
 
 			var _markdown = _content.ToMarkdown();
 
-			output.Content.SetHtmlContent(_markdown ?? string.Empty);
+			output.Content.SetHtmlContent(_markdown ?? String.Empty);
 		}
 	}
 }
