81ce0ce74f0fbde2d8119e958ddafb84e45c5489
Thomas Kindler <virtualdreamsdd@aol.com>
2020-04-23 15:09:02 +0200

    Moved AutoMapper mappings to Profiles.

--- Extensions/AutoMapperExtension.cs
+++ Extensions/AutoMapperExtension.cs
@@ -1,10 +1,5 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
-using System;
-using notes.Areas.Admin.Models;
-using notes.Core.Models;
-using notes.Models;
 
 namespace notes.Extensions
 {
@@ -12,29 +7,17 @@ namespace notes.Extensions
 	{
 		static public IServiceCollection AddAutoMapper(this IServiceCollection services)
 		{
-			var _autoMapperConfig = new MapperConfiguration(config =>
+			var mappingConfiguration = new MapperConfiguration(config =>
 			{
 				config.AllowNullCollections = false;
 
-				config.CreateMap<Note, NoteModel>()
-					.ForMember(d => d.Tags, map => map.MapFrom(s => s.Tags.Select(i => i.Name)))
-					.ForMember(d => d.TagsString, map => map.MapFrom(s => String.Join(" ", s.Tags.Select(i => i.Name))))
-					.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()))
-					.ForMember(d => d.Modified, map => map.MapFrom(s => s.Modified.ToLocalTime()));
-
-				config.CreateMap<User, UserModel>()
-					.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()));
-
-				config.CreateMap<Revision, RevisionModel>()
-					.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()))
-					.ForMember(d => d.Modified, map => map.MapFrom(s => s.Modified.ToLocalTime()));
-
-				config.CreateMap<DistinctAndCount, DistinctAndCountModel>();
+				config.AddMaps(typeof(Startup));
+				// config.AddProfile<MappingProfile>();
 			});
 
-			_autoMapperConfig.AssertConfigurationIsValid();
+			mappingConfiguration.AssertConfigurationIsValid();
 
-			services.AddSingleton<IMapper>(am => _autoMapperConfig.CreateMapper());
+			services.AddSingleton(mappingConfiguration.CreateMapper());
 
 			return services;
 		}
---
+++ MappingProfiles/MappingProfile.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using System.Linq;
+using System;
+using notes.Areas.Admin.Models;
+using notes.Core.Models;
+using notes.Models;
+
+namespace note.MappingProfiles
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			#region domain -> view
+			CreateMap<Note, NoteModel>()
+				.ForMember(d => d.Tags, map => map.MapFrom(s => s.Tags.Select(i => i.Name)))
+				.ForMember(d => d.TagsString, map => map.MapFrom(s => String.Join(" ", s.Tags.Select(i => i.Name))))
+				.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()))
+				.ForMember(d => d.Modified, map => map.MapFrom(s => s.Modified.ToLocalTime()));
+
+			CreateMap<User, UserModel>()
+				.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()));
+
+			CreateMap<Revision, RevisionModel>()
+				.ForMember(d => d.Created, map => map.MapFrom(s => s.Created.ToLocalTime()))
+				.ForMember(d => d.Modified, map => map.MapFrom(s => s.Modified.ToLocalTime()));
+
+			CreateMap<DistinctAndCount, DistinctAndCountModel>();
+			#endregion
+		}
+	}
+}
