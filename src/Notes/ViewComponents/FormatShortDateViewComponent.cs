7f5adab65dbb6a27d2c57e4c244d8f14c1cbbba9
Thomas Kindler <virtualdreamsdd@aol.com>
2021-06-11 16:31:31 +0200

    Added tooltip to user created in admin panel.
    Renamed FormatDateComponent and added new short version.
    Removed inline function.

---
+++ src/Notes/ViewComponents/FormatShortDateViewComponent.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Notes.Core.Interfaces;
+using System;
+
+namespace Notes.ViewComponents
+{
+	public class FormatShortDateViewComponent : BaseViewComponent
+	{
+		private readonly IUserService UserService;
+
+		public FormatShortDateViewComponent(IUserService user)
+			: base(user)
+		{
+			UserService = user;
+		}
+
+		public IViewComponentResult Invoke(DateTime date)
+		{
+			var _dt = String.Format(new System.Globalization.CultureInfo("en-US"), "{0:ddd, dd MMM yyyy}", date);
+
+			return Content($"{_dt}");
+		}
+	}
+}
