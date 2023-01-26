9b73f15917d39c00c341bbd916802bc29ad5433f
Thomas Kindler <virtualdreamsdd@aol.com>
2023-01-26 13:43:31 +0100

    Renamed MappingProfile to DomainViewProfile.

--- src/Notes/MappingProfiles/DomainViewProfile.cs
+++ src/Notes/MappingProfiles/DomainViewProfile.cs
@@ -7,11 +7,10 @@ using System;
 
 namespace Notes.MappingProfiles
 {
-	public class MappingProfile : Profile
+	public class DomainViewProfile : Profile
 	{
-		public MappingProfile()
+		public DomainViewProfile()
 		{
-			#region domain -> view
 			CreateMap<Note, NoteModel>()
 				.ForMember(d => d.Tags, map => map.MapFrom(s => s.Tags.Select(i => i.Name)))
 				.ForMember(d => d.TagsString, map => map.MapFrom(s => String.Join(" ", s.Tags.Select(i => i.Name))))
@@ -28,7 +27,6 @@ namespace Notes.MappingProfiles
 				.ForMember(d => d.Modified, map => map.MapFrom(s => s.Modified.ToLocalTime()));
 
 			CreateMap<DistinctAndCount, DistinctAndCountModel>();
-			#endregion
 		}
 	}
 }
