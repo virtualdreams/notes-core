40187ee8724b141fabcbc979aafb29b734032468
Thomas Kindler <virtualdreamsdd@aol.com>
2026-03-24 11:45:11 +0100

    Revert "Updated AutoMapper from 14.0.0 to 16.1.1."
    This reverts commit 316c85eab98352cd4a275c225c0d7938757ba692.

--- src/Notes/Extensions/AutoMapperExtensions.cs
+++ src/Notes/Extensions/AutoMapperExtensions.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Notes.Extensions
 {
@@ -14,7 +13,7 @@ namespace Notes.Extensions
 
 				config.AddMaps(typeof(Startup));
 				// config.AddProfile<MappingProfile>();
-			}, NullLoggerFactory.Instance);
+			});
 
 			mappingConfiguration.AssertConfigurationIsValid();
 
